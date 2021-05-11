e246eab981469f557dba0332f61a303c7302961e
frostornge <frostornge@gmail.com>
2021-05-11 07:26:26 +0000

    fix: schema generation script

--- contracts/staking/examples/schema.rs
+++ contracts/staking/examples/schema.rs
@@ -3,8 +3,8 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use pool::config::Config;
-use pool::msg::{HandleMsg, InitMsg, QueryMsg};
+use staking::msg;
+use staking::state;
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -12,8 +12,11 @@ fn main() {
     create_dir_all(&out_dir).unwrap();
     remove_schemas(&out_dir).unwrap();
 
-    export_schema(&schema_for!(InitMsg), &out_dir);
-    export_schema(&schema_for!(HandleMsg), &out_dir);
-    export_schema(&schema_for!(QueryMsg), &out_dir);
-    export_schema(&schema_for!(Config), &out_dir);
+    export_schema(&schema_for!(msg::InitMsg), &out_dir);
+    export_schema(&schema_for!(msg::HandleMsg), &out_dir);
+    export_schema(&schema_for!(msg::QueryMsg), &out_dir);
+    export_schema(&schema_for!(msg::Cw20HookMsg), &out_dir);
+    export_schema(&schema_for!(state::Config), &out_dir);
+    export_schema(&schema_for!(state::Reward), &out_dir);
+    export_schema(&schema_for!(state::User), &out_dir);
 }
