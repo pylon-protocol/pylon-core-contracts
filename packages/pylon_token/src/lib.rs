0521779642a48ad4c3e08a3661b5df2d1d8a9c73
frostornge <frostornge@gmail.com>
2021-07-29 14:07:48 +0900

    refactor(pylon_token): remove tax helpers

--- packages/pylon_token/src/lib.rs
+++ packages/pylon_token/src/lib.rs
@@ -4,12 +4,8 @@ pub mod common;
 pub mod community;
 pub mod distributor;
 pub mod gov;
-pub mod querier;
 pub mod staking;
 pub mod vesting;
 
 #[cfg(test)]
 mod mock_querier;
-
-#[cfg(test)]
-mod testing;
