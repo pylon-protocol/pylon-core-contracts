42296093d443e8ac5ed266235aa93427ff0ff685
frostornge <frostornge@gmail.com>
2021-07-29 11:29:05 +0900

    fix(pylon_core): change adapter interface as fabricator

--- packages/pylon_core/src/adapter.rs
+++ packages/pylon_core/src/adapter.rs
@@ -5,21 +5,11 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
-pub enum HandleMsg {
-    Deposit {},
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
-pub enum Cw20HookMsg {
-    Redeem {},
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
     Config {},
     ExchangeRate { input_denom: String },
+    Deposit { amount: Uint256 }, // input_denom
+    Redeem { amount: Uint256 },  // yield_token
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
