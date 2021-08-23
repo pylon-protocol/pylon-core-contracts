ca0eec3d1e902c698f01bdbd8601935e6aa2ee23
frostornge <frostornge@gmail.com>
2021-08-23 23:14:54 +0900

    fix: import for schema generation

--- contracts/gateway/pool/examples/schema.rs
+++ contracts/gateway/pool/examples/schema.rs
@@ -3,7 +3,7 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use launchpad_lockup::state::{Config, Reward, User};
+use gateway_pool::state::{config, state, user, withdrawal};
 use pylon_gateway::pool_msg::{HandleMsg, InitMsg, QueryMsg};
 
 fn main() {
@@ -15,7 +15,8 @@ fn main() {
     export_schema(&schema_for!(InitMsg), &out_dir);
     export_schema(&schema_for!(HandleMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
-    export_schema(&schema_for!(Config), &out_dir);
-    export_schema(&schema_for!(Reward), &out_dir);
-    export_schema(&schema_for!(User), &out_dir);
+    export_schema(&schema_for!(config::Config), &out_dir);
+    export_schema(&schema_for!(state::State), &out_dir);
+    export_schema(&schema_for!(user::User), &out_dir);
+    export_schema(&schema_for!(withdrawal::Withdrawal), &out_dir);
 }
