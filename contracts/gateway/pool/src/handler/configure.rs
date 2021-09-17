1d7806afc75f53702b0332af368163369f2d4880
frostornge <frostornge@gmail.com>
2021-09-17 23:11:47 +0900

    fix(gateway/pool): add test for message configure

--- contracts/gateway/pool/src/handler/configure.rs
+++ contracts/gateway/pool/src/handler/configure.rs
@@ -16,19 +16,60 @@ pub fn configure<S: Storage, A: Api, Q: Querier>(
     env: Env,
     msg: ConfigureMsg,
 ) -> StdResult<HandleResponse> {
+    let config = config::read(&deps.storage).unwrap();
+    validate_sender(&env, &config.owner, "configure")?;
+
     match msg {
-        ConfigureMsg::Owner { address } => configure_owner(deps, env, address),
+        ConfigureMsg::Owner { address: owner } => {
+            let mut config = config::read(&deps.storage).unwrap();
+
+            let prev_owner = config.owner;
+            config.owner = owner;
+
+            config::store(&mut deps.storage, &config).unwrap();
+
+            Ok(HandleResponse {
+                messages: vec![],
+                log: vec![
+                    log("action", "configure_owner"),
+                    log("prev_owner", prev_owner),
+                    log("next_owner", config.owner),
+                ],
+                data: None,
+            })
+        }
         ConfigureMsg::Deposit {
             start,
             finish,
             user_cap,
             total_cap,
-        } => configure_deposit(deps, env, start, finish, user_cap, total_cap),
+        } => {
+            let mut config = config::read(&deps.storage).unwrap();
+            let mut deposit_config = config.deposit_config;
+
+            let mut logs = vec![log("action", "configure_deposit")];
+            logs.append(&mut deposit_config.time.configure(start, finish));
+            if let Some(user_cap) = user_cap {
+                deposit_config.user_cap = user_cap;
+                logs.push(log("new_user_cap", user_cap));
+            }
+            if let Some(total_cap) = total_cap {
+                deposit_config.total_cap = total_cap;
+                logs.push(log("new_total_cap", total_cap));
+            }
+
+            config.deposit_config = deposit_config;
+            config::store(&mut deps.storage, &config).unwrap();
+
+            Ok(HandleResponse {
+                messages: vec![],
+                log: logs,
+                data: None,
+            })
+        }
         ConfigureMsg::Withdraw { strategy } => {
             let mut config = config::read(&deps.storage).unwrap();
-            validate_sender(&env, &config.owner, "configure_withdraw")?;
 
-            print!("{}, {}", strategy.len(), MAX_WITHDRAW_STRATEGY);
             if strategy.len().gt(&MAX_WITHDRAW_STRATEGY) {
                 return Err(StdError::generic_err(format!(
                     "Lockup: withdraw strategy length exceeds limit. limit: {}, now: {}",
@@ -56,7 +97,6 @@ pub fn configure<S: Storage, A: Api, Q: Querier>(
         ConfigureMsg::Claim { start, finish } => {
             let mut config = config::read(&deps.storage).unwrap();
             let mut claim_time = config.claim_time;
-            validate_sender(&env, &config.owner, "configure_claim")?;
 
             let mut logs = vec![log("action", "configure_claim")];
             logs.append(&mut claim_time.configure(start, finish));
@@ -70,77 +110,12 @@ pub fn configure<S: Storage, A: Api, Q: Querier>(
                 data: None,
             })
         }
-        ConfigureMsg::Distribution(msg) => configure_distribution(deps, env, msg),
-    }
-}
-
-fn configure_owner<S: Storage, A: Api, Q: Querier>(
-    deps: &mut Extern<S, A, Q>,
-    env: Env,
-    owner: HumanAddr,
-) -> StdResult<HandleResponse> {
-    let mut config = config::read(&deps.storage).unwrap();
-    validate_sender(&env, &config.owner, "configure_owner")?;
-
-    let prev_owner = config.owner;
-    config.owner = owner;
-
-    config::store(&mut deps.storage, &config).unwrap();
-
-    Ok(HandleResponse {
-        messages: vec![],
-        log: vec![
-            log("action", "configure_owner"),
-            log("prev_owner", prev_owner),
-            log("next_owner", config.owner),
-        ],
-        data: None,
-    })
-}
-
-fn configure_deposit<S: Storage, A: Api, Q: Querier>(
-    deps: &mut Extern<S, A, Q>,
-    env: Env,
-    start: Option<u64>,
-    finish: Option<u64>,
-    user_cap: Option<Uint256>,
-    total_cap: Option<Uint256>,
-) -> StdResult<HandleResponse> {
-    let mut config = config::read(&deps.storage).unwrap();
-    let mut deposit_config = config.deposit_config;
-    validate_sender(&env, &config.owner, "configure_deposit")?;
-
-    let mut logs = vec![log("action", "configure_deposit")];
-    logs.append(&mut deposit_config.time.configure(start, finish));
-    if let Some(user_cap) = user_cap {
-        deposit_config.user_cap = user_cap;
-        logs.push(log("new_user_cap", user_cap));
-    }
-    if let Some(total_cap) = total_cap {
-        deposit_config.total_cap = total_cap;
-        logs.push(log("new_total_cap", total_cap));
-    }
-
-    config.deposit_config = deposit_config;
-    config::store(&mut deps.storage, &config).unwrap();
-
-    Ok(HandleResponse {
-        messages: vec![],
-        log: logs,
-        data: None,
-    })
-}
-
-fn configure_distribution<S: Storage, A: Api, Q: Querier>(
-    deps: &mut Extern<S, A, Q>,
-    env: Env,
-    msg: DistributionMsg,
-) -> StdResult<HandleResponse> {
-    match msg {
-        DistributionMsg::AddReward { amount } => adjust_reward(deps, env, amount, false),
-        DistributionMsg::SubReward { amount } => adjust_reward(deps, env, amount, true),
-        DistributionMsg::LengthenPeriod { time } => adjust_period(deps, env, time, false),
-        DistributionMsg::ShortenPeriod { time } => adjust_period(deps, env, time, true),
+        ConfigureMsg::Distribution(msg) => match msg {
+            DistributionMsg::AddReward { amount } => adjust_reward(deps, env, amount, false),
+            DistributionMsg::SubReward { amount } => adjust_reward(deps, env, amount, true),
+            DistributionMsg::LengthenPeriod { time } => adjust_period(deps, env, time, false),
+            DistributionMsg::ShortenPeriod { time } => adjust_period(deps, env, time, true),
+        },
     }
 }
 
@@ -154,7 +129,6 @@ fn adjust_reward<S: Storage, A: Api, Q: Querier>(
 
     let mut config = config::read(&deps.storage).unwrap();
     let mut dist_config = config.distribution_config;
-    validate_sender(&env, &config.owner, action)?;
 
     if remove && env.block.time.gt(&dist_config.time.finish) {
         return Err(StdError::generic_err(format!(
@@ -170,8 +144,8 @@ fn adjust_reward<S: Storage, A: Api, Q: Querier>(
             .finish
             .sub(dist_config.applicable_start_time(env.block.time)),
     );
-    if env.block.time.gt(&dist_config.time.start) {
-        dist_config.reward_rate = if remove {
+    if remove {
+        dist_config.reward_rate = if env.block.time.gt(&dist_config.time.start) {
             Decimal256::from_uint256(
                 dist_config
                     .reward_rate
@@ -180,6 +154,13 @@ fn adjust_reward<S: Storage, A: Api, Q: Querier>(
                     .div(Decimal256::from_uint256(remaining)),
             )
         } else {
+            dist_config
+                .reward_rate
+                .sub(Decimal256::from_uint256(amount).div(Decimal256::from_uint256(remaining)))
+        };
+        dist_config.total_reward_amount = dist_config.total_reward_amount.sub(amount);
+    } else {
+        dist_config.reward_rate = if env.block.time.gt(&dist_config.time.start) {
             Decimal256::from_uint256(
                 dist_config
                     .reward_rate
@@ -187,14 +168,13 @@ fn adjust_reward<S: Storage, A: Api, Q: Querier>(
                     .add(amount)
                     .div(Decimal256::from_uint256(remaining)),
             )
+        } else {
+            dist_config
+                .reward_rate
+                .add(Decimal256::from_uint256(amount).div(Decimal256::from_uint256(remaining)))
         };
-    } else {
-        dist_config.reward_rate = dist_config.reward_rate.add(Decimal256::from_uint256(
-            amount.div(Decimal256::from_uint256(remaining)),
-        ));
+        dist_config.total_reward_amount = dist_config.total_reward_amount.add(amount);
     }
-    dist_config.total_reward_amount = dist_config.total_reward_amount.add(amount);
-
     config.distribution_config = dist_config;
     config::store(&mut deps.storage, &config).unwrap();
 
@@ -223,7 +203,6 @@ fn adjust_period<S: Storage, A: Api, Q: Querier>(
 
     let mut config = config::read(&deps.storage).unwrap();
     let mut dist_config = config.distribution_config;
-    validate_sender(&env, &config.owner, action)?;
 
     let reward_rate_before = dist_config.reward_rate;
     let finish_time_before = dist_config.time.finish;
@@ -235,22 +214,19 @@ fn adjust_period<S: Storage, A: Api, Q: Querier>(
     );
 
     dist_config.reward_rate = if shorten {
-        Decimal256::from_uint256(
-            dist_config
-                .reward_rate
-                .mul(remaining)
-                .div(Decimal256::from_uint256(remaining.sub(Uint256::from(time)))),
-        )
+        dist_config.time.finish = dist_config.time.finish.sub(time);
+        dist_config
+            .reward_rate
+            .mul(Decimal256::from_uint256(remaining))
+            .div(Decimal256::from_uint256(remaining.sub(Uint256::from(time))))
     } else {
-        Decimal256::from_uint256(
-            dist_config
-                .reward_rate
-                .mul(remaining)
-                .div(Decimal256::from_uint256(remaining.add(Uint256::from(time)))),
-        )
+        dist_config.time.finish = dist_config.time.finish.add(time);
+        dist_config
+            .reward_rate
+            .mul(Decimal256::from_uint256(remaining))
+            .div(Decimal256::from_uint256(remaining.add(Uint256::from(time))))
     };
 
-    dist_config.time.finish = dist_config.time.finish.add(time);
     config.distribution_config = dist_config;
     config::store(&mut deps.storage, &config).unwrap();
 
--- contracts/gateway/pool/src/testing/mod.rs
+++ contracts/gateway/pool/src/testing/mod.rs
@@ -1,5 +1,6 @@
 mod constants;
 mod test_handle_claim;
+mod test_handle_configure;
 mod test_handle_deposit;
 mod test_handle_update;
 mod test_handle_withdraw;
