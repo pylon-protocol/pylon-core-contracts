178cab24977e299c5e1aa6f44ab265b53eb4c1b9
frostornge <frostornge@gmail.com>
2021-10-07 03:31:29 +0900

    correct decimal error

--- contracts/gateway/swap/src/testing/test_querier_strategy.rs
+++ contracts/gateway/swap/src/testing/test_querier_strategy.rs
@@ -71,3 +71,65 @@ fn strategy_claimable_token_of() {
         );
     }
 }
+
+#[test]
+fn strategy_claimable_token_of_valkyrie() {
+    let mut deps = mock_dependencies(&[]);
+
+    config::store(deps.as_mut().storage)
+        .save(&config::Config {
+            owner: "".to_string(),
+            beneficiary: "".to_string(),
+            price: Default::default(),
+            start: 0,
+            finish: 0,
+            cap_strategy: None,
+            distribution_strategy: vec![
+                Strategy::Lockup {
+                    release_time: 1,
+                    release_amount: Decimal256::from_str("0.33").unwrap(),
+                },
+                Strategy::Lockup {
+                    release_time: 2,
+                    release_amount: Decimal256::from_str("0.33").unwrap(),
+                },
+                Strategy::Lockup {
+                    release_time: 3,
+                    release_amount: Decimal256::from_str("0.34").unwrap(),
+                },
+            ],
+            whitelist_enabled: false,
+            swap_pool_size: Default::default(),
+        })
+        .unwrap();
+
+    let user_addr = &deps.api.addr_canonicalize(TEST_USER_1).unwrap();
+    user::store(
+        deps.as_mut().storage,
+        user_addr,
+        &user::User {
+            whitelisted: false,
+            swapped_in: Default::default(),
+            swapped_out: Uint256::from(50000000000u64),
+            swapped_out_claimed: Default::default(),
+        },
+    )
+    .unwrap();
+
+    println!(
+        "{}",
+        strategy::claimable_token_of(deps.as_ref(), 0, TEST_USER_1.to_string()).unwrap()
+    );
+    println!(
+        "{}",
+        strategy::claimable_token_of(deps.as_ref(), 1, TEST_USER_1.to_string()).unwrap()
+    );
+    println!(
+        "{}",
+        strategy::claimable_token_of(deps.as_ref(), 2, TEST_USER_1.to_string()).unwrap()
+    );
+    println!(
+        "{}",
+        strategy::claimable_token_of(deps.as_ref(), 3, TEST_USER_1.to_string()).unwrap()
+    );
+}
