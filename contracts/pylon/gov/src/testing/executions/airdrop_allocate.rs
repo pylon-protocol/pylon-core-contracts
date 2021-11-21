ed558157c65f8aeece135ed2a93d5f69c73f3b3c
frostornge <frostornge@gmail.com>
2021-11-22 01:45:42 +0900

    test: add airdrop test

--- contracts/pylon/gov/src/testing/executions/airdrop_allocate.rs
+++ contracts/pylon/gov/src/testing/executions/airdrop_allocate.rs
@@ -1,10 +1,12 @@
-use cosmwasm_std::{Env, MessageInfo, Uint128};
+use cosmwasm_std::testing::{mock_env, mock_info};
+use cosmwasm_std::{attr, Api, Env, MessageInfo, Uint128};
 
+use crate::error::ContractError;
 use crate::executions::airdrop::allocate;
 use crate::executions::ExecuteResult;
-use crate::testing::MockDeps;
+use crate::state::airdrop::Reward;
+use crate::testing::{mock_deps, MockDeps, TEST_CREATOR, TEST_TOKEN, TEST_VOTER};
 
-#[allow(dead_code)]
 pub fn exec(
     deps: &mut MockDeps,
     env: Env,
@@ -21,4 +23,58 @@ pub fn exec(
         recipient,
         allocate_amount,
     )
+}
+
+#[test]
+fn success() {
+    let mut deps = mock_deps();
+    super::instantiate::default(&mut deps);
+    super::airdrop_instantiate::default(&mut deps, TEST_TOKEN, 1000);
+
+    let response = exec(
+        &mut deps,
+        mock_env(),
+        mock_info(TEST_CREATOR, &[]),
+        0,
+        TEST_VOTER.to_string(),
+        Uint128::from(1234u128),
+    )
+    .unwrap();
+    assert_eq!(
+        response.attributes,
+        vec![
+            attr("action", "airdrop_allocate"),
+            attr("airdrop_id", 0.to_string()),
+            attr("recipient", TEST_VOTER),
+            attr("amount", Uint128::from(1234u128))
+        ]
+    );
+
+    let reward = Reward::load(
+        &deps.storage,
+        &deps.api.addr_validate(TEST_VOTER).unwrap(),
+        &0u64,
+    )
+    .unwrap();
+    assert_eq!(reward.reward, Uint128::from(1234u128));
+}
+
+#[test]
+fn fail_unauthorized() {
+    let mut deps = mock_deps();
+    super::instantiate::default(&mut deps);
+    super::airdrop_instantiate::default(&mut deps, TEST_TOKEN, 1000);
+
+    match exec(
+        &mut deps,
+        mock_env(),
+        mock_info(TEST_VOTER, &[]),
+        0,
+        TEST_VOTER.to_string(),
+        Uint128::from(1234u128),
+    ) {
+        Ok(_) => panic!("Must return error"),
+        Err(ContractError::Unauthorized {}) => (),
+        Err(e) => panic!("Unexpected error: {:?}", e),
+    }
 }
--- contracts/pylon/gov/src/testing/executions/airdrop_update.rs
+++ contracts/pylon/gov/src/testing/executions/airdrop_update.rs
@@ -1,10 +1,11 @@
-use cosmwasm_std::{Env, MessageInfo};
+use cosmwasm_std::testing::{mock_env, mock_info, MOCK_CONTRACT_ADDR};
+use cosmwasm_std::{attr, Api, Decimal, Env, MessageInfo, Uint128};
 
 use crate::executions::airdrop::update;
 use crate::executions::ExecuteResult;
-use crate::testing::MockDeps;
+use crate::state::airdrop::{Airdrop, Reward};
+use crate::testing::{mock_deps, mock_env_height, MockDeps, TEST_TOKEN, TEST_VOTER, VOTING_TOKEN};
 
-#[allow(dead_code)]
 pub fn exec(
     deps: &mut MockDeps,
     env: Env,
@@ -12,4 +13,119 @@ pub fn exec(
     target: Option<String>,
 ) -> ExecuteResult {
     update(deps.as_mut(), env, info, target)
+}
+
+#[test]
+fn success() {
+    let mut deps = mock_deps();
+    super::instantiate::default(&mut deps);
+    let (env, _, _) = super::airdrop_instantiate::default(&mut deps, TEST_TOKEN, 86400);
+
+    let response = exec(
+        &mut deps,
+        env.clone(),
+        mock_info(TEST_VOTER, &[]),
+        Some(TEST_VOTER.to_string()),
+    )
+    .unwrap();
+    assert_eq!(
+        response.attributes,
+        vec![
+            attr("action", "airdrop_update"),
+            attr("updated", "[0]".to_string())
+        ]
+    );
+
+    let reward = Reward::load(
+        &deps.storage,
+        &deps.api.addr_validate(TEST_VOTER).unwrap(),
+        &0,
+    )
+    .unwrap();
+    assert_eq!(reward.reward_per_token_paid, Decimal::zero());
+
+    deps.querier.with_token_balances(&[(
+        &VOTING_TOKEN.to_string(),
+        &[(&MOCK_CONTRACT_ADDR.to_string(), &Uint128::from(100u128))],
+    )]);
+
+    super::staking_deposit::exec(
+        &mut deps,
+        mock_env(),
+        mock_info(MOCK_CONTRACT_ADDR, &[]),
+        TEST_VOTER.to_string(),
+        Uint128::from(100u128),
+    )
+    .unwrap();
+
+    let response = exec(
+        &mut deps,
+        mock_env_height(env.block.height, env.block.time.seconds() + 100),
+        mock_info(TEST_VOTER, &[]),
+        Some(TEST_VOTER.to_string()),
+    )
+    .unwrap();
+    assert_eq!(
+        response.attributes,
+        vec![
+            attr("action", "airdrop_update"),
+            attr("updated", "[0]".to_string())
+        ]
+    );
+
+    let airdrop = Airdrop::load(&deps.storage, &0).unwrap();
+    assert_eq!(
+        airdrop.state.last_update_time,
+        env.block.time.seconds() + 100
+    );
+    assert_eq!(airdrop.state.reward_per_token_stored, Decimal::one());
+
+    let reward = Reward::load(
+        &deps.storage,
+        &deps.api.addr_validate(TEST_VOTER).unwrap(),
+        &0,
+    )
+    .unwrap();
+    assert_eq!(reward.reward, Uint128::from(100u128));
+    assert_eq!(reward.reward_per_token_paid, Decimal::one());
+
+    let response = exec(
+        &mut deps,
+        mock_env_height(
+            env.block.height,
+            env.block.time.seconds() + airdrop.config.period * 2,
+        ),
+        mock_info(TEST_VOTER, &[]),
+        Some(TEST_VOTER.to_string()),
+    )
+    .unwrap();
+    assert_eq!(
+        response.attributes,
+        vec![
+            attr("action", "airdrop_update"),
+            attr("updated", "[0]".to_string())
+        ]
+    );
+
+    let airdrop = Airdrop::load(&deps.storage, &0).unwrap();
+    assert_eq!(
+        airdrop.state.last_update_time,
+        env.block.time.seconds() + airdrop.config.period
+    );
+    assert_eq!(
+        airdrop.state.reward_per_token_stored,
+        Decimal::from_ratio(86400u128, 100u128)
+    );
+
+    let reward = Reward::load(
+        &deps.storage,
+        &deps.api.addr_validate(TEST_VOTER).unwrap(),
+        &0,
+    )
+    .unwrap();
+    assert_eq!(reward.reward, Uint128::from(86400u128));
+    assert_eq!(
+        reward.reward_per_token_paid,
+        Decimal::from_ratio(86400u128, 100u128)
+    );
 }
